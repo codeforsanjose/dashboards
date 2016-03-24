484a2e85549e299e3ca7c1396459aa9189147c31
golgistudio <lauriesreynolds@gmail.com>
2016-03-23 18:07:15 -0700

    data explorer for fire data using dc.js and leaflet

---
+++ dashboards/fire-data-explorer/data/fireStations.js
@@ -0,0 +1,305 @@
+var fireStations = {
+
+    "raw_data": [
+        {
+            name: "Station 1",
+            latitude:37.338735,
+            longitude:-121.894996,
+            address: "225 North Market Street",
+            zipCode: 95110,
+            battalion: 1,
+            station: 1
+        },
+        {
+            name: "Station 2",
+            latitude:37.364630,
+            longitude:-121.831449,
+            address: "2949 Alum Rock Avenue",
+            zipCode: 95127,
+            battalion: 2,
+            station: 2
+        },
+
+        {
+            name: "Station 3",
+            latitude:37.323285,
+            longitude:-121.878515,
+            address: "98 Martha Street",
+            zipCode: 95112,
+            battalion: 1,
+            station: 3
+        },
+
+        {
+            name: "Station 4",
+            latitude:37.314556,
+            longitude:-121.923112,
+            address: "710 Leigh Avenue",
+            zipCode: 95128,
+            battalion: 10,
+            station: 4
+        },
+
+        {
+            name: "Station 5",
+            latitude:37.365804,
+            longitude:-121.899346,
+            address: "1380 North 10th Street",
+            zipCode: 95112,
+            battalion: 29,
+            station: 5
+        },
+        {
+            name: "Station 6",
+            latitude:37.301451,
+            longitude:-121.903757,
+            address: "1386 Cherry Avenue",
+            zipCode: 95125,
+            battalion: 10,
+            station: 6
+        },
+        {
+            name: "Station 7",
+            latitude:37.341265,
+            longitude:-121.916733,
+            address: "800 Emory Street",
+            zipCode: 95126,
+            battalion: 1,
+            station: 7
+        },
+        {
+            name: "Station 8",
+            latitude:37.343726,
+            longitude:-121.873983,
+            address: "800 Emory Street",
+            zipCode: 95112,
+            battalion: 1,
+            station: 8
+        },
+        {
+            name: "Station 9",
+            latitude:37.263435,
+            longitude:-121.916393,
+            address: "3410 Ross Avenue",
+            zipCode: 95112,
+            battalion: 10,
+            station: 9
+        },
+        {
+            name: "Station 10",
+            latitude:37.318180,
+            longitude:-121.943728,
+            address: "511 South Monroe Street",
+            zipCode: 95128,
+            battalion: 10,
+            station: 10
+        },
+        {
+            name: "Station 11",
+            latitude:37.291754,
+            longitude:-121.755250,
+            address: "The Villages Parkway",
+            zipCode: 95135,
+            battalion: 2,
+            station: 11
+        },
+        {
+            name: "Station 12",
+            latitude:37.242954,
+            longitude:-121.848940,
+            address: "5912 Cahalan Avenue",
+            zipCode: 95123,
+            battalion: 13,
+            station: 12
+        },
+        {
+            name: "Station 13",
+            latitude:37.267401,
+            longitude:-121.866402,
+            address: "4380 Pearl Avenue",
+            zipCode: 95136,
+            battalion: 13,
+            station: 13
+        },
+        {
+            name: "Station 14",
+            latitude:37.302456,
+            longitude:-121.979804,
+            address: "1201 San Tomas Aquino Road",
+            zipCode: 95117,
+            battalion: 10,
+            station: 14
+        },
+        {
+            name: "Station 15",
+            latitude:37.301448,
+            longitude:-122.023413,
+            address: "1248 Blaney Avenue",
+            zipCode: 95129,
+            battalion: 10,
+            station: 15
+        },
+        {
+            name: "Station 16",
+            latitude:37.327864,
+            longitude:-121.831850,
+            address: "2001 South King Road",
+            zipCode: 95122,
+            battalion: 10,
+            station: 16
+        },
+        {
+            name: "Station 17",
+            latitude:37.247271,
+            longitude:-121.885245,
+            address: "5170 Coniston Way",
+            zipCode: 95118,
+            battalion: 13,
+            station: 17
+        },
+        {
+            name: "Station 18",
+            latitude:37.272903,
+            longitude:-121.828768,
+            address: "4430 Monterey Highway",
+            zipCode: 95111,
+            battalion: 13,
+            station: 18
+        },
+        {
+            name: "Station 19",
+            latitude:37.399324,
+            longitude:-121.847238,
+            address: "3292 Sierra Road",
+            zipCode: 95132,
+            battalion: 2,
+            station: 19
+        },
+        {
+            name: "Station 20",
+            latitude:37.358528,
+            longitude:-121.917835,
+            address: "1433 Airport Boulevard",
+            zipCode: 95110,
+            battalion: 29,
+            station: 20
+        },
+        {
+            name: "Station 21",
+            latitude:37.351726,
+            longitude:-121.799157,
+            address: "1749 Mount Pleasant Road",
+            zipCode: 95148,
+            battalion: 2,
+            station: 21
+        },
+        {
+            name: "Station 22",
+            latitude:37.222044,
+            longitude:-121.867660,
+            address: "6461 Bose Lane",
+            zipCode: 95120,
+            battalion: 13,
+            station: 22
+        },
+        {
+            name: "Station 23",
+            latitude:37.401745,
+            longitude:-121.879119,
+            address: "1771 Via Cinco de Mayo",
+            zipCode: 95132,
+            battalion: 29,
+            station: 23
+        },
+        {
+            name: "Station 24",
+            latitude:37.299565,
+            longitude:-121.801941,
+            address: "1924 Yerba Buena Road",
+            zipCode: 95121,
+            battalion: 2,
+            station: 24
+        },
+        {
+            name: "Station 25",
+            latitude:37.426513,
+            longitude:-121.969198,
+            address: "1525 Wilson Way",
+            zipCode: 95002,
+            battalion: 29,
+            station: 25
+        },
+        {
+            name: "Station 26",
+            latitude:37.306629,
+            longitude:-121.849020,
+            address: "528 Tully Road",
+            zipCode: 95111,
+            battalion: 1,
+            station: 26
+        },
+        {
+            name: "Station 27",
+            latitude:37.232738,
+            longitude:-121.771762,
+            address: "6027 San Ignacio Road",
+            zipCode: 95119,
+            battalion: 13,
+            station: 27
+        },
+        {
+            name: "Station 28",
+            latitude:37.199576,
+            longitude:-121.824689,
+            address: "19911 McKean Road",
+            zipCode: 95120,
+            battalion: 13,
+            station: 28
+        },
+        {
+            name: "Station 29",
+            latitude:37.401099,
+            longitude:-121.933839,
+            address: "199 Innovation Drive",
+            zipCode: 95134,
+            battalion: 29,
+            station: 29
+        },
+        {
+            name: "Station 30",
+            latitude:37.325196,
+            longitude:-121.895509,
+            address: "454 Auzerais",
+            zipCode: 95126,
+            battalion: 1,
+            station: 30
+        },
+        {
+            name: "Station 31",
+            latitude:37.315246,
+            longitude:-121.774444,
+            address: "3100 Ruby Avenue",
+            zipCode: 95135,
+            battalion: 2,
+            station: 31
+        },
+        {
+            name: "Station 34",
+            latitude:37.360482,
+            longitude:-121.867494,
+            address: "1634 Las Plumas Avenue",
+            zipCode: 95133,
+            battalion: 29,
+            station: 34
+        },
+        {
+            name: "Station 35",
+            latitude:37.251809,
+            longitude:-121.805489,
+            address: "135 Poughkeepsie Road",
+            zipCode: 95123,
+            battalion: 13,
+            station: 35
+        }
+]}
+
